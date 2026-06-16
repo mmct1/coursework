57cc6f0e19663ae4f9b7de7aad1702378eda0f89
mmct1 <mary.26.mctarsney@gmail.com>
2026-06-16 09:40:27 -0400

    ngrams finished

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -7,5 +7,4 @@
 
 
 
- zcat googlebooks-eng-all-1gram-20120701-1.gz | egrep '^(18[0-9][0-9]|19[0-9][0-9]|20[0-9][0-9])'$'\t' > year_counts.tsv
-
+zcat googlebooks-eng-all-1gram-20120701-1.gz | egrep '^(18[0-9][0-9]|19[0-9][0-9]|20[0-9][0-9])'$'\t' > year_counts.tsv
--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -4,4 +4,6 @@
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
 
-cat googlebooks-eng-all-totalcounts-20120701.txt | tr '\t' '\n' > total_counts.csv
+# cat googlebooks-eng-all-totalcounts-20120701.txt | tr '\t' '\n' > total_counts.csv
+# below is probably an easier/better way to do this
+sed 's/\t/\n/g' googlebooks-eng-all-totalcounts-20120701.txt >  total_counts.csv
